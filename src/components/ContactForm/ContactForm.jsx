94d63272d85e1ddc9c2bd1469ef5d9f831b3e248
uadarksamurai <itandeducationua@gmail.com>
2023-06-23 12:52:19 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -11,7 +11,7 @@ export const ContactForm = ({ onSubmit, nameInputId, numberInputId }) => {
         type="text"
         name="name"
         className={css.inputstyle}
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         id={nameInputId}
         required
@@ -24,7 +24,7 @@ export const ContactForm = ({ onSubmit, nameInputId, numberInputId }) => {
         type="tel"
         name="number"
         className={css.inputstyle}
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         id={numberInputId}
         required
